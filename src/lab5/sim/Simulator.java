8381406de6ac0f383869ed6212e6dff81ba213e0
sambjo <36854245+sambjo@users.noreply.github.com>
2018-02-27 13:30:48 +0100

    ändringar i sim och event

--- src/lab5/sim/Simulator.java
+++ src/lab5/sim/Simulator.java
@@ -1,5 +1,18 @@
 package lab5.sim;
 
-public class Simulator {
+import lab5.smallStore.SmallStoreState;
+
+public class Simulator { 
+	private SmallStoreState state;
+	private EventQueue eventQueue;
+	public Simulator() {
+		this.state= new SmallStoreState();
+		this.eventQueue =new EventQueue();
+	}
+	public void run() {
+		while (state.stopFlag==false) {
+			while ()
+		}
+	}
 
 }
