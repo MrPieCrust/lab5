f3fb89aeb28402c7caaaaec13ecfbdd854a5dd73
JPerssoon <jesperpersson97@hotmail.com>
2018-02-27 19:59:52 +0100

    fifiostuuff

--- src/lab5/smallStore/FIFO.java
+++ src/lab5/smallStore/FIFO.java
@@ -8,19 +8,17 @@ import lab5.smallStore.customer.Customer;
 
 public class FIFO{
 	private ArrayList<Customer> regQueue = new ArrayList<Customer>();
-<<<<<<< HEAD
 	private ArrayList<Register> registers = new ArrayList<Register>();
 	private SmallStoreState state;
 	int freeRegisters; 
 	int openRegisters;
-=======
+
 	private ArrayList<Integer> nrOfReg = new ArrayList<Integer>();
 	private SmallStoreState state;
 	private boolean isEmpty1 = true;
 	private boolean isEmpty2 = true;
 	private boolean isEmpty3 = true;
 	private boolean isEmpty4 = true;
->>>>>>> b5d3b3b9492e65529e2e4b380979edcfe97a7692
 
 	public FIFO(SmallStoreState state) {
 		this.state = state;
@@ -96,7 +94,7 @@ class Register{
 			open=true;
 		}
 	}
-<<<<<<< HEAD
+
 	void changeFree() {
 		if(free) {
 			free = false;
@@ -111,34 +109,30 @@ class Register{
 	public boolean isFree() {
 		return free;
 	}
-=======
-	
-	
-	public void regStat() {
-        if(nrOfReg.size() <= state.maxRegisters) {
-        //int counter = 0;
-            if(regQueue.size() >= 0) {
-                    if(isEmpty1 == true) {
-                        removeFirst(); //tar bort först ur kön
-                        isEmpty1 = false;
-                        state.payed();
-                        isEmpty = true;
-                    } else if(isEmpty2 == true) {
-                        removeFirst(); 
-                        isEmpty1 = false;
-                    } else if(isEmpty3 == true) {
-                        removeFirst(); 
-                        isEmpty1 = false;
-                    } else if(isEmpty2 == true) {
-                        removeFirst(); 
-                        isEmpty1 = false;
-                    }
-            }
 
-<<<<<<< HEAD
-        }
-    }
-=======
->>>>>>> b5d3b3b9492e65529e2e4b380979edcfe97a7692
->>>>>>> b3145b867a6915f4be7ad72d2ad0b06c6b4bf939
-}
+	
+//	
+//	public void regStat() {
+//        if(nrOfReg.size() <= state.maxRegisters) {
+//        //int counter = 0;
+//            if(regQueue.size() >= 0) {
+//                    if(isEmpty1 == true) {
+//                        removeFirst(); //tar bort först ur kön
+//                        isEmpty1 = false;
+//                        state.payed();
+//                        isEmpty = true;
+//                    } else if(isEmpty2 == true) {
+//                        removeFirst(); 
+//                        isEmpty1 = false;
+//                    } else if(isEmpty3 == true) {
+//                        removeFirst(); 
+//                        isEmpty1 = false;
+//                    } else if(isEmpty2 == true) {
+//                        removeFirst(); 
+//                        isEmpty1 = false;
+//                    }
+//            }
+//
+//        }
+//    }
+
