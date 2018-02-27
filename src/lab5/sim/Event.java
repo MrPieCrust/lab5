ccbc133ad7979ff81e864ac8d77277ddab2b9364
sambjo <36854245+sambjo@users.noreply.github.com>
2018-02-27 10:47:08 +0100

    adderat i event

--- src/lab5/sim/Event.java
+++ src/lab5/sim/Event.java
@@ -1,5 +1,7 @@
 package lab5.sim;
 
 public class Event {
-
+	public void addElapsedTime(double currentTime){
+		State.timeElapsed+=currentTime;
+	}
 }
--- src/lab5/sim/State.java
+++ src/lab5/sim/State.java
@@ -5,6 +5,6 @@ import java.util.Observable;
 public class State extends Observable {
 	public final int fseed=5;
 	public boolean stopFlag = false;
-	public double timeElapsed = 0; //elapsed time.
-	public double runTime =480; //runtime for sim
+	public static double timeElapsed = 0; //elapsed time.
+	public double runTime =480; //runtime for sim in minutes.
 }
